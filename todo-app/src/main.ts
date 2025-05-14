9a14f75e61b8fc77810f36c7318c4064fcf700a5
Chastine12 <Chastine12@gmail.com>
2025-05-14 11:12:54 +0800

    Exam

--- todo-app/src/main.ts
+++ todo-app/src/main.ts
@@ -1,6 +1,15 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
+import { importProvidersFrom } from '@angular/core';
 import { AppComponent } from './app/app.component';
+import { appConfig } from './app/app.config';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+// Material and HTTP imports
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+bootstrapApplication(AppComponent, {
+  providers: [
+    ...appConfig.providers,
+    importProvidersFrom(HttpClientModule, BrowserAnimationsModule)
+  ]
+});
